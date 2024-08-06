d1e8365c2a13e28154b8cc91552543fea2e8f046
Nick Ater <30247698+nickater@users.noreply.github.com>
2024-08-05 23:08:07 -0400

    can navigate to question and prediction

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,15 +1,25 @@
 import { CustomSafeAreaView } from "@/components";
 import { useAuth } from "@/hooks/auth";
+import { router } from "expo-router";
 import React from "react";
-import { Button, StyleSheet, Text } from "react-native";
+import { Button, StyleSheet } from "react-native";
 
 export default () => {
   const { signOut } = useAuth();
 
+  const handleNavigateToPrediction = () => {
+    router.navigate("prediction");
+  };
+
+  const handleNavigateToQuestion = () => {
+    router.navigate("question");
+  };
+
   return (
     <CustomSafeAreaView>
-      <Text>Logged In</Text>
       <Button title="Sign Out" onPress={signOut} />
+      <Button title="Prediction" onPress={handleNavigateToPrediction} />
+      <Button title="Question" onPress={handleNavigateToQuestion} />
     </CustomSafeAreaView>
   );
 };
